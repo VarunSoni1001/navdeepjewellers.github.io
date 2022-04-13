9e881e7472eb8899e24feee224dd8f1c0afd40c5
Varun Soni <varun1001soni@gmail.com>
2022-04-13 16:59:40 +0530

    14

--- js/resp.js
+++ js/resp.js
@@ -1,12 +1,14 @@
 function myFunction(x) {
-    x.classList.toggle("change");
-  }
+  x.classList.toggle("change");
+}
 
-//Get the button:
+
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -16,8 +18,8 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}  
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
